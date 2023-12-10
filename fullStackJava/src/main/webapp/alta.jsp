6fb08b83b5771fdd98bb3a264c212f13c4b86719
Cam <142924881+camilaparoni@users.noreply.github.com>
2023-12-10 15:17:23 -0300

    Add files via upload

---
+++ fullStackJava/src/main/webapp/alta.jsp
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <%@include file="head.jsp" %>
+    </head>
+
+    <body data-bs-theme="dark">  
+        <main>
+            <!-- ACA VA EL NAVBAR  -->
+            <%@include file="navbar.jsp" %>
+
+            <div class="container">                 
+                <section class="col-6 mx-auto">
+                    <div class="text-center mt-4" id="formulario">
+                        <medium>Conviértete en un</medium>
+                        <h2>ORADOR</h2>
+                        <p>Anótate como orador para dar una <u>charla ignite.</u> Cuéntanos de qué quieres hablar!</p>
+                    </div>
+                    <form method="post" id="alta" action="<%=request.getContextPath()%>/CreateController">
+                        <div class="d-flex flex-row mb-3">
+                            <input name= "nombre" type="text" class="form-control me-2" id="nombre" placeholder="Nombre">
+                            <input name="apellido" type="text" class="form-control" id="apellido" placeholder="Apellido">
+                        </div>
+                        <div class="d-flex flex-row mb-3">
+                            <input name= "mail" type="text" class="form-control" id="mail" placeholder="Correo">
+                        </div>
+
+                        <div class="mb-3 mx-auto">
+                            <textarea name="tema" class="form-control" id="exampleFormControlTextarea1" placeholder="¿Sobre qué quieres hablar?"
+                                      rows="3"></textarea>
+                            <small class="text-left text-secondary">Recuerda incluir un título para tu charla</small>
+                        </div>
+                        <button type="submit" class="btn btn-success bg-gradient col-12"> Enviar</button>
+                        <script>
+                            let alta = document.getElementById("alta");
+                            let nombre = document.getElementById("nombre");
+                            let apellido = document.getElementById("apellido");
+                            let mail = document.getElementById("mail");
+                            let tema = document.getElementById("exampleFormControlTextarea1");
+                            alta.addEventListener("submit", (e) => {
+                                e.preventDefault();
+                                if (confirm("Desea dar de alta el siguiente orador:\n" +
+                                        "Nombre y Apellido: " + nombre.value + " " + apellido.value + "\n" +
+                                        "Mail: " + mail.value + "\n" +
+                                        "Tema: " + tema.value
+                                        ) === true) {
+                                    alta.submit();
+                                }
+                            });
+                        </script>
+                    </form>
+                </section>
+            </div>
+
+        </main>
+        <%@include file="footer.jsp" %>
+    </body>	
+</html>
---
+++ fullStackJava/src/main/webapp/assets/js/validar.js
@@ -0,0 +1,87 @@
+
+function validarNombre(event) {
+    var nombre = document.getElementById("nombre").value;
+    var mensajeError = document.getElementById("mensajeError");
+
+    // Verificar si el campo está vacío
+    if (nombre === "") {
+      mensajeError.innerHTML = "Este campo no puede estar vacío.";
+      event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+    } else {
+      // Verificar si el campo contiene números
+      if (/\d/.test(nombre)) { //busca números decimales en el nombre, si hay entra y da error
+        mensajeError.innerHTML = "Este campo no puede contener números.";
+        event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+      }
+    }
+ //}
+ 
+}
+function validarApellido(event) {
+    var apellido = document.getElementById('apellido').value;
+    var mensajeErrorApellido = document.getElementById('mensajeErrorApellido');
+
+    // Verificar si el campo está vacío
+    if (apellido === "") {
+      mensajeErrorApellido.innerHTML = "Este campo no puede estar vacío.";
+      event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+    } else {
+      // Verificar si el campo contiene números
+      if (/\d/.test(apellido)) {
+        mensajeErrorApellido.innerHTML = "Este campo no puede contener números.";
+        event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+      }
+    }
+  }
+
+
+
+function validarEmail(event) {
+    var email = document.getElementById('correo').value;
+    var mensajeErrorEmail = document.getElementById('mensajeErrorEmail');
+    if (email === "") {
+      mensajeErrorEmail.innerHTML = "Este campo no puede estar vacío.";
+      event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+    } else {
+      if (!/^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/.test(email)) {
+        mensajeErrorEmail.innerHTML = "El correo electrónico no es válido.";
+        event.preventDefault();
+      }
+    }
+  }
+
+function validarResumen (event) {
+  var nombre = document.getElementById("nombre").value;
+  var apellido = document.getElementById('apellido').value;
+  var email = document.getElementById('correo').value;
+  var resumen = document.getElementById('resumen');
+  var cantidad = document.getElementById('inputCantidad')
+  var categoria = document.getElementById("inputState")
+  var mensajeErrorResumen = document.getElementById('mensajeErrorResumen')
+  if (nombre === "" || apellido === "" || email === "" || cantidad === "" || categoria === "") {
+    mensajeErrorResumen.innerHTML = "No puede haber campos vacíos, verifique.";
+    event.preventDefault(); // Evitar el comportamiento por defecto (envío del formulario)
+  }
+}
+
+function borrarMensajeError() {
+  var mensajeError = document.getElementById("mensajeError");
+  mensajeError.innerHTML = ""; // Borra el mensaje de error
+}
+function borrarMensajeErrorApellido() {
+  var mensajeErrorApellido = document.getElementById("mensajeErrorApellido");
+  mensajeErrorApellido.innerHTML = ""; // Borra el mensaje de error
+}
+
+function borrarMensajeErrorEmail() {
+  var mensajeErrorEmail = document.getElementById("mensajeErrorEmail");
+  mensajeErrorEmail.innerHTML = ""; // Borra el mensaje de error
+}
+
+
+
+
+
+
+
+
